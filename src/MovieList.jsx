b1eb3c674189dafcd2a20d355e550054e3de8934
unknown <JNKarif>
2024-02-02 18:18:48 +0600

    movie data

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,4 +1,7 @@
+import star from "../src/assets/Images/star.svg";
 import Sidebar from "./Sidebar";
+
+import thumbnail from "../src/assets/movie-covers/iron-man.png";
 
 export default function MovieList() {
   return (
@@ -11,22 +14,18 @@ export default function MovieList() {
           <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-7">
             {/* <!-- Begin Card --> */}
             <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-              <img
-                className="w-full object-cover"
-                src="./assets/movie-1.png"
-                alt=""
-              />
+              <img className="w-full object-cover" src={thumbnail} alt="" />
               <figcaption className="pt-4">
                 <h3 className="text-xl mb-1">Iron Man</h3>
                 <p className="text-[#575A6E] text-sm mb-2">
                   Action/Adventure/Sci-fi
                 </p>
                 <div className="flex items-center space-x-1 mb-5">
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
                 </div>
                 <a
                   className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
@@ -41,22 +40,18 @@ export default function MovieList() {
 
             {/* <!-- Begin Card --> */}
             <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-              <img
-                className="w-full object-cover"
-                src="./assets/movie-1.png"
-                alt=""
-              />
+              <img className="w-full object-cover" src={thumbnail} alt="" />
               <figcaption className="pt-4">
                 <h3 className="text-xl mb-1">Iron Man</h3>
                 <p className="text-[#575A6E] text-sm mb-2">
                   Action/Adventure/Sci-fi
                 </p>
                 <div className="flex items-center space-x-1 mb-5">
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
                 </div>
                 <a
                   className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
@@ -71,22 +66,18 @@ export default function MovieList() {
 
             {/* <!-- Begin Card --> */}
             <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-              <img
-                className="w-full object-cover"
-                src="./assets/movie-1.png"
-                alt=""
-              />
+              <img className="w-full object-cover" src={thumbnail} alt="" />
               <figcaption className="pt-4">
                 <h3 className="text-xl mb-1">Iron Man</h3>
                 <p className="text-[#575A6E] text-sm mb-2">
                   Action/Adventure/Sci-fi
                 </p>
                 <div className="flex items-center space-x-1 mb-5">
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
                 </div>
                 <a
                   className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
@@ -101,22 +92,18 @@ export default function MovieList() {
 
             {/* <!-- Begin Card --> */}
             <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-              <img
-                className="w-full object-cover"
-                src="./assets/movie-1.png"
-                alt=""
-              />
+              <img className="w-full object-cover" src={thumbnail} alt="" />
               <figcaption className="pt-4">
                 <h3 className="text-xl mb-1">Iron Man</h3>
                 <p className="text-[#575A6E] text-sm mb-2">
                   Action/Adventure/Sci-fi
                 </p>
                 <div className="flex items-center space-x-1 mb-5">
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
                 </div>
                 <a
                   className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
@@ -131,22 +118,18 @@ export default function MovieList() {
 
             {/* <!-- Begin Card --> */}
             <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-              <img
-                className="w-full object-cover"
-                src="./assets/movie-1.png"
-                alt=""
-              />
+              <img className="w-full object-cover" src={thumbnail} alt="" />
               <figcaption className="pt-4">
                 <h3 className="text-xl mb-1">Iron Man</h3>
                 <p className="text-[#575A6E] text-sm mb-2">
                   Action/Adventure/Sci-fi
                 </p>
                 <div className="flex items-center space-x-1 mb-5">
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
                 </div>
                 <a
                   className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
@@ -161,22 +144,18 @@ export default function MovieList() {
 
             {/* <!-- Begin Card --> */}
             <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-              <img
-                className="w-full object-cover"
-                src="./assets/movie-1.png"
-                alt=""
-              />
+              <img className="w-full object-cover" src={thumbnail} alt="" />
               <figcaption className="pt-4">
                 <h3 className="text-xl mb-1">Iron Man</h3>
                 <p className="text-[#575A6E] text-sm mb-2">
                   Action/Adventure/Sci-fi
                 </p>
                 <div className="flex items-center space-x-1 mb-5">
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
-                  <img src="./assets/star.svg" width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
+                  <img src={star} width="14" height="14" alt="" />
                 </div>
                 <a
                   className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
