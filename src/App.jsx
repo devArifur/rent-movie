a99971d1a29534d37fd5ec75a161a8fcca0a429a
unknown <JNKarif>
2024-02-03 11:08:03 +0600

    shopping cart added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /**
  * Component Hirerchy:
  *  Main
@@ -11,16 +12,27 @@
  *
  */
 
+import { useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import MovieList from "./MovieList";
+import Sidebar from "./Sidebar";
+import MovieList from "./cine/MovieList";
+import { MovieContext } from "./context";
 
 export default function App() {
+  const [cartData, setCartData] = useState([]);
   return (
     <>
-      <Header />
-      <MovieList />
-      <Footer />
+      <MovieContext.Provider value={{ cartData, setCartData }}>
+        <Header />
+        <main>
+          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+            <Sidebar />
+            <MovieList />
+          </div>
+        </main>
+        <Footer />
+      </MovieContext.Provider>
     </>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,30 @@
+import { useContext, useState } from "react";
 import logo from "../src/assets/Images/logo.svg";
 import ring from "../src/assets/Images/ring.svg";
 import cart from "../src/assets/Images/shopping-cart.svg";
 import moon from "../src/assets/icons/moon.svg";
+import CardDetails from "./cine/CardDetails";
+import { MovieContext } from "./context";
+
 export default function Header() {
+  const [showCart, setShowCart] = useState(false);
+  const { cartData } = useContext(MovieContext);
+
+  console.log(cartData);
+
+  function handleShowCart() {
+    setShowCart(true);
+  }
+
+  function handleCartClose() {
+    setShowCart(false);
+  }
+
   return (
     <header>
-      <nav className="container flex items-center justify-between space-x-10 py-6">
+      {showCart && <CardDetails onClose={handleCartClose} />}
+
+      <nav className="container  flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
           <img src={logo} width="139" height="26" alt="" />
         </a>
@@ -31,8 +50,14 @@ export default function Header() {
             <a
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
+              onClick={handleShowCart}
             >
               <img src={cart} width="24" height="24" alt="" />
+              {cartData.length > 0 && (
+                <span className="rounded-full absolute top-[-12px] left-[28px] bg-[#12CF6F] text-white text-center p-[2px] w-[30px] h-[30px]">
+                  {cartData.length}
+                </span>
+              )}
             </a>
           </li>
         </ul>
